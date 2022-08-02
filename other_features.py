ee94effc037627f4b0d18904a330fcca2eca7141
Alien501 <97647867+Alien501@users.noreply.github.com>
2022-08-02 07:37:21 +0530

    Update other_features.py

--- other_features.py
+++ other_features.py
@@ -33,3 +33,46 @@ def lyrics():
         lyrics()
     else:
         return
+    
+# Function to port playlist from pc to android
+def port_play():
+    while True:
+        play_loc = input('Enter playlist path: ').strip()
+        if '\\' not in play_loc or play_loc[-4:].lower() != '.m3u' or len(play_loc) == 0:
+            print('Enter a valid path. . .')
+            continue
+        break
+    try:
+        with open(play_loc, 'r') as f:
+            data = f.readlines()
+            print('Sample location from playlist file:', data[2], sep='\n')
+    except:
+        print(f'File {play_loc} not found. . .')
+        return
+
+    while True:
+        rep  = input('Enter common path from above: ').strip()
+        if '\\' not in rep or len(rep) == 0:
+            print("Enter a valid path. . .")
+            continue
+        else:
+            while True:
+                tex = input('Enter common path in new device: ').strip()
+                if '\\' not in tex or len(tex) == 0:
+                    print("Enter a valid path. . .")
+                    continue
+                break
+            break
+    play_loc = play_loc[:-4]+' NEW '+play_loc[-4:]
+
+    with open(play_loc, 'w') as f:
+        for i in data:
+            if rep in i:
+                i = i.replace(rep, tex)
+                i = i.split('\\\\')
+                i = '/'.join(i)
+                f.write(i)
+            else:
+                f.write(i)
+    print('Replaced. . .')
+
